fc83036e029547d2f43fbc6a9103310b70433e42
Hemant dev <54200516+INTRUDER1@users.noreply.github.com>
2020-11-07 20:49:02 +0530

    update axios.js

--- amazon-clone/src/axios.js
+++ amazon-clone/src/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   // THE API (cloud function) URL
   baseURL: '..........'
+     // You will get this link terminal after typing [firebase emulator-start] and then option to click in there. Then copy and paste this localhost:5001 url in baseURL column.
     // "http://localhost:5001/challenge-4b2b2/us-central1/api",
 });
 

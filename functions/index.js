7a6725b6f7f6fb8965c5269ce8b6ca3d83898886
Hemant dev <54200516+INTRUDER1@users.noreply.github.com>
2020-11-07 20:42:40 +0530

    update index.js

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,33 @@ const cors = require("cors");
 const { response, request } = require('express');
 const stripe = require("stripe")("sk_test_51HixlNEUSLvodRlWzVVmnhoAjp7jL4UZiuqtY9THUlliqSmPpKzqvcKYvph2M0midbBlL6ZiQvXK4GckrfIFw6RV00GStJIlxi")
 
+// API
 
+// - App config
+const app = express();
 
-const
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "usd",
+  });
+
+  // OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
